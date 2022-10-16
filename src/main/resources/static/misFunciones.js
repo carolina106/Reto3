d7b8ceca1a6a99bea56c2e278f22168e9deadb5d
carolina106 <carolina.rodriguez.mt@usa.edu.co>
2022-10-15 20:21:35 -0500

    ip

---
+++ src/main/resources/static/misFunciones.js
@@ -0,0 +1,33 @@
+function traerDatosCategoria(){
+    $.ajax({
+        url:"http://150.136.56.226/api/Category/all",
+        type: "GET",
+        dataType: "json",
+        success: function(respuesta){
+            pintarDatos(respuesta);
+        },
+        error: function(respuesta, xhr){
+            alert("Error de petición");
+        }
+    })
+}
+
+function pintarDatos(datos){
+    let html="";
+    html+= "<tr>";
+    Object.keys(datos[0]).forEach(elemento => {
+        html+="<th>"+elemento+"</th>";
+    })
+    html+="</tr>"
+
+    for(let i=0;i<datos.length;i++){
+        html+="<tr>";
+        Object.values(datos[i]).forEach(elemento => {
+            html+="<td>"+elemento+"</td>";
+        })
+        html+="</tr>";
+    }
+
+    $("#tabla").empty();
+    $("#tabla").append(html);
+}
